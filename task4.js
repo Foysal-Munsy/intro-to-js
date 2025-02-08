5c60b337795cafc3cb9d9b96784cfbe028905228
Foysal-Munsy <bdfoysal555@gmail.com>
2025-02-08 12:26:20 +0600

    task 4

--- task4.js
+++ task4.js
@@ -1,8 +1,8 @@
 // What will be the result of the following codes:
 
 // var a = isNaN(‘11’);
+// it will give ans false, since 11 is a number.
 
 // var a = isNaN(2-10);
+// it will give ans false, since 2-10=-8 is a number.
 
-// Explain your answers.
-//  ans:
